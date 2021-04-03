75a3fb56582c699e7c78bd1893323a9d133c048c
augoodman <augoodman@icloud.com>
2021-04-02 17:23:59 -0700

    act1 nearly complete

--- activity1/routes/index.js
+++ activity1/routes/index.js
@@ -3,19 +3,77 @@ var router = express.Router();
 var fs = require('fs');
 const article = fs.readFileSync('articles/article.txt', 'UTF-8');
 const lines = article.split(/\r?\n/);
+var comments = [{'id': 1, 'comment': 'first'}, {'id': 69, 'comment': 'hello'}];
+var activities = ['test1','test2','test3'];
 var title = '';
 var body = '';
-var comments = [];
+
+var reset = function(){
+  activities = []
+  console.log('User activities reset.')
+}
+
+var add = function(id, comment) {
+  //add handling for duplicate id values
+  if(!('id' in comments)) {
+    comments.push({'id': id, 'comment': comment});
+  }
+}
+
+var del = function(id) {
+  //add handling for key !exist
+  delete comments[id];
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  for (let i = 0; i < lines.length; i++) {
-    if (i === 0) {
-      title = lines[i];
-    } else {
-      body += lines[i];
+  if(body === ''){
+    for (let i = 0; i < lines.length; i++) {
+      if (i === 0) {
+        title = lines[i];
+      } else {
+        body += lines[i];
+      }
     }
   }
+  res.render('index', {
+    title: title,
+    article: body,
+    numComments: comments.length,
+    comments: comments
+  });
+});
+
+/* GET view page. */
+router.get('/view', function(req, res, next) {
+  res.render('view', {
+    title: 'View User Activity',
+    activities: activities,
+  });
+});
+
+router.get('/reset', function(req, res, next) {
+  reset();
+  res.render('index', {
+    title: title,
+    article: body,
+    numComments: comments.length,
+    comments: comments
+  });
+});
+
+router.post('/add', function(req, res, next) {
+  add(req.body.id, req.body.comment);
+  res.render('index', {
+    title: title,
+    article: body,
+    numComments: comments.length,
+    comments: comments
+  });
+});
+
+router.post('/delete', function(req, res, next) {
+  del(req.body.id);
   res.render('index', {
     title: title,
     article: body,
--- activity1/routes/users.js
+++ activity1/routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET view listing. */
+router.get('/users', function(req, res, next) {
+  res.render('view', {
+
+  });
 });
 
 module.exports = router;
