fc15b483bc9e9b9669f53fecf77ff91c648169c5
augoodman <augoodman@icloud.com>
2021-04-10 22:38:44 -0700

    lab4 complete

--- activity2/README.md
+++
@@ -1,7 +0,0 @@
-# Lab 4
-
-## Activity 1
-
-## Activity 2
-
-## Activity 3
--- activity3/activity3.js
+++ activity3/activity3.js
@@ -1,10 +1,10 @@
 /**
- * File: activity2.js
+ * File: activity3.js
  * SER 421
  * Lab 4
  *
  * This file implements a Javascript-based roommate finder service as specified in
- * Activity 2.
+ * Activity 3.
  *
  * Functions are:
  *    startup()
@@ -12,9 +12,13 @@
  *    comment()
  *    idle()
  *    isJson(string)
+ *    command(comment)
+ *    clearCookies()
  */
-/* global variables */const session = {'username': undefined, 'status': false};
+/* global variables */
+const session = {'username': undefined, 'status': false};
 let timer = {};
+let searchHistory = [];
 let dictionary = {
   "dictionary_name": "default",
   "entries":
@@ -38,6 +42,7 @@ let dictionary = {
         "answer": ["Can you reply something?", "You have been idle for more than 30 seconds", "Whats the matter with you? Submit something"]
       }]
 };
+let workingCopy = dictionary;
 let reviewTitle = "LEGO 45";
 let reviewBody = "This is going to be a tremendous review. No one has ever seen a review this great. It's about a " +
     "great man, a genius really. And he's in a world make of these little plastic bricks. I think they call them " +
@@ -56,7 +61,6 @@ let review = [
 ];
 let commentForm = '<textarea id="comment" rows="4" cols="30" placeholder="Please enter a comment"></textarea>' +
     '<br><input type="button" name="comment" value="Comment" onclick="comment()">';
-
 /* functions */
 /*******************************************************************************************
  * startup() - Takes user to initial state. Varies if user cookies are found.
@@ -84,6 +88,7 @@ function startup(){
     ];
     document.getElementById('review').innerHTML = review[1];
     document.getElementById('comment_form').innerHTML = commentForm;
+    document.getElementById('comment').value = localStorage.getItem(session.username);
     alert('Welcome back ' + username + "!");
   }
 }
@@ -111,6 +116,7 @@ function login(){
   ];
   document.getElementById('review').innerHTML = review[1];
   document.getElementById('comment_form').innerHTML = commentForm;
+  document.getElementById('comment').value = localStorage.getItem(session.username);
 }
 
 /*******************************************************************************************
@@ -128,6 +134,10 @@ function comment(){
   timer = idle();
   let lastWord = '';
   let comment = document.getElementById('comment').value;
+  if(comment.startsWith('/')){  //check if comment is a command
+    command(comment);
+    return;
+  }
   if(comment.startsWith('{') || comment.endsWith('}')){ //check if comment is a JSON update
     if(isJson(comment)){
       let jsonComment = JSON.parse(comment);
@@ -148,6 +158,17 @@ function comment(){
   }
   let commentWords = comment.replace(/[^a-zA-Z -]/g, '').split(' '); //strip non-letters out of comment
   let newComment = '';
+  let count;
+  if(sessionStorage.getItem('reviews') === null){ //store uncensored reviews
+    let reviews = {'uncensored':[], 'censored':[]};
+    reviews.uncensored.push(commentWords);
+    sessionStorage.setItem('reviews', JSON.stringify(reviews));
+  }
+  else{
+    let reviews = JSON.parse(sessionStorage.getItem('reviews'));
+    reviews.uncensored.push(commentWords);
+    sessionStorage.setItem('reviews', JSON.stringify(reviews));
+  }
   for(let i in dictionary.entries){ //search dictionary and censor comment as appropriate
     let numAnswers = dictionary.entries[i].answer.length;
     for(let j in dictionary.entries[i].key){
@@ -158,6 +179,8 @@ function comment(){
             commentWords[k] = dictionary.entries[i].answer[Math.floor((Math.random() * numAnswers))];
           }
           lastWord = commentWords[k];
+          count++;
+          sessionStorage.setItem(session.username, count.toString());
         }
       }
     }
@@ -165,7 +188,11 @@ function comment(){
   for(let i in commentWords){ //build censored comment string
     newComment += commentWords[i] + ' ';
   }
+  let reviews = JSON.parse(sessionStorage.getItem('reviews'));
+  reviews.censored.push(commentWords);
+  sessionStorage.setItem('reviews', JSON.stringify(reviews)); //store censored reviews
   document.getElementById('comment').value = newComment;
+  localStorage.setItem(session.username, newComment); //make stateful by storing user's last comment
 }
 
 /*******************************************************************************************
@@ -208,4 +235,100 @@ function isJson(string){
     return false;
   }
   return true;
+}
+
+/*******************************************************************************************
+ * command(comment) - Parses commands and takes appropriate action.
+ *
+ * arguments:
+ *   string - command to be executed
+ *
+ * returns:
+ *   nothing
+ */
+function command(comment){
+  let command = comment.substring(1); //strip leading '/'
+  if(command === 'clear'){  //execute clear command by clearing all storage and resetting DOM elements
+    clearCookies();
+    localStorage.clear();
+    sessionStorage.clear();
+    session.status = false;
+    document.getElementById('comment_form').innerHTML = '';
+    document.getElementById('history').innerHTML = '';
+    document.getElementById('reviews').innerHTML = '';
+    startup();
+  }
+  if(command.indexOf('search') !== -1){ //parse search value and execute search
+    let value = [];
+    let results = [];
+    let resultString = '';
+    value = comment.split(' ');
+    searchHistory.push(value[1]);
+    for(let i in dictionary.entries){
+      for(let j in dictionary.entries[i].key){
+        if(value[1] === dictionary.entries[i].key[j]){
+          for(let l in dictionary.entries[i].answer) {
+            results.push(dictionary.entries[i].answer[l]);
+          }
+        }
+      }
+    }
+    for(let i = 0; i < results.length; i++){
+      if(i < results.length - 1) {
+        resultString += results[i] + ', ';
+      }
+      else{
+        resultString += results[i];
+      }
+    }
+    document.getElementById('comment').value = resultString;
+  }
+  if(command === 'history'){  //execute history command by inserting ordered list of search terms into the DOM
+    let history = '<ol>';
+    for(let i in searchHistory){
+      history += '<li>' + searchHistory[i] + '</li>';
+    }
+    history += '</ol>';
+    document.getElementById('history').innerHTML = history;
+  }
+  if(command === 'count'){  //execute count command by displaying numbers of censored words
+    if(sessionStorage.getItem(session.username) === null){
+      alert('You need to submit a comment before you can get a count');
+      return;
+    }
+    document.getElementById('comment').value = 'Number of rude words used is ' + sessionStorage.getItem(session.username);
+  }
+  if(command === 'list'){ //execute list command by parsing sessionStorage into JSON and inserting review lists into the DOM
+    if(sessionStorage.getItem('reviews') === null){
+      alert('You need to submit a review before you can get a list of reviews');
+      return;
+    }
+    let reviewString = 'List of reviews:<br>';
+    let uncensored = JSON.parse(sessionStorage.getItem('reviews')).uncensored;
+    for(let i in uncensored){
+      reviewString += '<li>' + uncensored[i] + '</li>';
+    }
+    reviewString += '<br>List of censored reviews:<br>';
+    let censored = JSON.parse(sessionStorage.getItem('reviews')).censored;
+    for(let i in censored){
+      reviewString += '<li>' + censored[i] + '</li>';
+    }
+    document.getElementById('reviews').innerHTML = reviewString;
+  }
+}
+
+/*******************************************************************************************
+ * clearCookies - Clears user cookies.
+ *
+ * arguments:
+ *   string - command to be executed
+ *
+ * returns:
+ *   nothing
+ */
+function clearCookies(){
+  let cookies = document.cookie.split(';');
+  for(let i in cookies){
+    document.cookie = cookies[i] + "=;expires=" + new Date(0).toUTCString();
+  }
 }
